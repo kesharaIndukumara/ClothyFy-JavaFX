ea4dff7ebd94200e08f294a0418e78a228ae6b66
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-10-05 10:27:05 +0530

    Add login functionality to StoreDashboardController for user and admin

--- src/main/java/edu/icet/ecom/controller/StoreDashboardController.java
+++ src/main/java/edu/icet/ecom/controller/StoreDashboardController.java
@@ -1,4 +1,28 @@
 package edu.icet.ecom.controller;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StoreDashboardController {
+
+    @FXML
+    void loginasuserOnAction(ActionEvent event) throws IOException {
+        Stage s1 = new Stage();
+        s1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/UserLoginInterface.fxml"))));
+        s1.show();
+    }
+
+    @FXML
+    void loginasadminOnAction(ActionEvent event) throws IOException {
+        Stage s1 = new Stage();
+        s1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminLoginInterface.fxml"))));
+        s1.show();
+    }
+
 }
