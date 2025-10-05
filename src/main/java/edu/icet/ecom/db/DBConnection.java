f02364b2226b6822f220e9d475cd0b4acd6bb630
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-10-05 10:29:32 +0530

    Add login functionality to StoreDashboardController for user and admin

---
+++ src/main/java/edu/icet/ecom/db/DBConnection.java
@@ -0,0 +1,27 @@
+package edu.icet.ecom.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothify_store", "root", "1234");
+        }
+        return connection;
+    }
+}
